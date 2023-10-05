b6e85d45ac0c5d9bedbb3f2c83068b57727b20bd
Abu Bakar Adni <108950455+adnijufr1@users.noreply.github.com>
2023-10-05 16:42:46 +0700

    Update dim_table_postgres.py

--- airflow/dags/modules/dim_table_postgres.py
+++ airflow/dags/modules/dim_table_postgres.py
@@ -24,11 +24,7 @@ engine_postgre = create_engine(postgres_conn_string)
 
 
 
-df_status = pd.read_sql("""SELECT * FROM status_class;""", con=engine_mysql)  
-
-dim_status = df_status.copy()
-dim_status.to_sql(name='dim_status_table', index=False , con=engine_postgre, if_exists ='replace')
-
+# Dim Province
 
 df_province = pd.read_sql("""SELECT kode_prov,nama_prov FROM covid_jabar;
 """, con=engine_mysql)
@@ -39,7 +35,7 @@ dim_province.rename(columns = new_column_names_dim_prov, inplace=True)
 dim_province.drop_duplicates(inplace=True)
 dim_province.to_sql(name='dim_provice_table', con=engine_postgre, index=False, if_exists='replace')
 
-
+# Dim District
 df_district = pd.read_sql("""SELECT DISTINCT(kode_kab), nama_kab,nama_prov from covid_jabar;
 """, con=engine_mysql)
 
